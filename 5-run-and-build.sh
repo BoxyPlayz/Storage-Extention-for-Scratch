09d9bf7715e6eb144e466731b84fa2849ec3b961
Boxy <137698834+BoxyPlayz@users.noreply.github.com>
2024-01-03 20:17:06 +0000

    Update

---
+++ 5-run-and-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Verifying location of Scratch source is known"
+if [ -z "$SCRATCH_SRC_HOME" ]; then
+    echo "Error: SCRATCH_SRC_HOME environment variable is not set."
+    exit 1
+fi
+
+echo "BUILDING SCRATCH VM ..."
+cd $SCRATCH_SRC_HOME/scratch-vm
+NODE_OPTIONS='--openssl-legacy-provider' ./node_modules/.bin/webpack --bail
+
+echo "BUILDING SCRATCH GUI ..."
+cd $SCRATCH_SRC_HOME/scratch-gui
+NODE_OPTIONS='--openssl-legacy-provider' ./node_modules/.bin/webpack --bail
+
+python3 -m http.server -d $SCRATCH_SRC_HOME/scratch-gui/build
