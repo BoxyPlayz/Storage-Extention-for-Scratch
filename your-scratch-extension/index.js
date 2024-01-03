9980413685615f35751df9fe11879b2e5717e033
Boxy <137698834+BoxyPlayz@users.noreply.github.com>
2024-01-03 19:35:55 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -1,15 +1,15 @@
+
 const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
-const { syllable } = require('syllable');
+
+const FileSaver = require('file-saver');
+  
 
 class Scratch3YourExtension {
 
     constructor (runtime) {
-        import('syllable')
-        .then((syllableModule) => {
-            this.syllable = syllableModule.syllable;
-        })
+        // put any setup for your extension here
     }
 
     /**
@@ -18,37 +18,42 @@ class Scratch3YourExtension {
     getInfo () {
         return {
             // unique ID for your extension
-            id: 'yourScratchExtension',
+            id: 'downloaderExtention',
 
             // name that will be displayed in the Scratch UI
-            name: 'Test',
-
-
+            name: 'Download Files',
 
             // colours to use for your extension blocks
             color1: '#000099',
             color2: '#660066',
 
-            //Images
+            // icons to display
             blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
             menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
 
+            description: (
+                <FormattedMessage
+                    defaultMessage="Storage"
+                    description="Local Storage Addon"
+                    id="gui.extension.storage.description"
+                />
+            ),
 
             // your Scratch blocks
             blocks: [
                 {
                     // name of the function where your block code lives
-                    opcode: 'syllableBlock',
+                    opcode: 'downloadBlock',
 
                     // type of block - choose from:
                     //   BlockType.REPORTER - returns a value, like "direction"
                     //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
                     //   BlockType.COMMAND - a normal command block, like "move {} steps"
                     //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
-                    blockType: BlockType.REPORTER,
+                    blockType: BlockType.COMMAND,
 
                     // label to display on the block
-                    text: 'Syllables in [MY_TEXT]',
+                    text: 'Download [TEXT] as [FILE_NAME]',
 
                     // true if this block should end a stack
                     terminal: false,
@@ -61,16 +66,125 @@ class Scratch3YourExtension {
 
                     // arguments used in the block
                     arguments: {
-                        MY_TEXT: {
-                            defaultValue: 'I am a lie',
+                        TEXT: {
+                            // default value before the user sets something
+                            defaultValue: 'Hello World!',
 
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
                             type: ArgumentType.STRING
                         },
+                        FILE_NAME: {
+                            // default value before the user sets something
+                            defaultValue: 'download.txt',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
+                    }
+                },/*
+                {
+                    // name of the function where your block code lives
+                    opcode: 'uploadBlock',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'Open File as Text',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        empty: {
+                            defaultValue: 'empty',
+
+                            type: ArgumentType.STRING
+                        }
+                    }
+                },
+                */
+                {
+                    
+                    // name of the function where your block code lives
+                    opcode: 'keySetBlock',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.COMMAND,
+
+                    // label to display on the block
+                    text: 'Set [TEXT] to [KEY] in localstorage',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        TEXT: {
+                            // default value before the user sets something
+                            defaultValue: 'Value',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        },
+                        KEY: {
+                            // default value before the user sets something
+                            defaultValue: 'Key',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
                     }
                 },
                 {
+                    
                     // name of the function where your block code lives
-                    opcode: 'myFirstBlock',
+                    opcode: 'keyGetBlock',
 
                     // type of block - choose from:
                     //   BlockType.REPORTER - returns a value, like "direction"
@@ -80,7 +194,7 @@ class Scratch3YourExtension {
                     blockType: BlockType.REPORTER,
 
                     // label to display on the block
-                    text: 'Title for [BOOK_NUMBER] in ISBN Library',
+                    text: 'Get [KEY] from localstorage',
 
                     // true if this block should end a stack
                     terminal: false,
@@ -93,12 +207,105 @@ class Scratch3YourExtension {
 
                     // arguments used in the block
                     arguments: {
-                        BOOK_NUMBER: {
-                            defaultValue: '9781465444820',
+                        KEY: {
+                            // default value before the user sets something
+                            defaultValue: 'Key',
 
-                            type: ArgumentType.NUMBER
-                        },
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
                     }
+                
+                },
+                {
+                    
+                    // name of the function where your block code lives
+                    opcode: 'keyDeleteBlock',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.COMMAND,
+
+                    // label to display on the block
+                    text: 'Delete [KEY] from localstorage',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        KEY: {
+                            // default value before the user sets something
+                            defaultValue: 'Key',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
+                    }
+                
+                },
+                {
+                    
+                    // name of the function where your block code lives
+                    opcode: 'keyExistsBlock',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.BOOLEAN,
+
+                    // label to display on the block
+                    text: '[KEY] Exists in localstorage',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        KEY: {
+                            // default value before the user sets something
+                            defaultValue: 'Key',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
+                    }
+                
                 },
             ]
         };
@@ -109,25 +316,55 @@ class Scratch3YourExtension {
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
      */
-    myFirstBlock ({BOOK_NUMBER}) {
+    downloadBlock ({ TEXT, FILE_NAME }) {
         // example implementation to return a string
-        return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
-        .then((response) => {
-            if (response.ok){
-                return response.json();
-            }
-            else {
-                return {title: 'unknown'};
-            }
-        })
-        .then ((bookinfo) => {
-            return bookinfo.title;
-        })
+        var file = new Blob(
+            [TEXT], 
+            {
+                type: "text/plain;charset=utf-8"
+            });
+        FileSaver.saveAs(file, FILE_NAME);
     }
 
-    syllableBlock ({MY_TEXT}) {
-        return this.syllable(MY_TEXT);
+    keySetBlock({KEY, TEXT}) {
+        localStorage.setItem('SCRATCHKEY' + KEY, TEXT);
     }
+
+    keyGetBlock({KEY}) {
+        let item = localStorage.getItem('SCRATCHKEY' + KEY);
+        if (item != null){
+            return localStorage.getItem('SCRATCHKEY' + KEY);
+        }
+        else{
+            return '';
+        }
+    }
+    keyDeleteBlock({KEY}) {
+        let item = localStorage.getItem('SCRATCHKEY' + KEY);
+        if (item != null){
+            localStorage.removeItem('SCRATCHKEY' + KEY)
+        }
+    }
+    keyExistsBlock({KEY}) {
+        if (localStorage['SCRATCHKEY' + KEY]){
+            return true;
+        }
+        else{
+            return false;
+        };
+
+
+    /*uploadBlock ({empty}) {
+        let fileHandle = window.showOpenFilePicker();
+        let fileData = fileHandle.getFile();
+        let fileText = fileData.text();
+
+        return fileText
+    }
+    */
+
+
+    }    
 }
 
 module.exports = Scratch3YourExtension;
